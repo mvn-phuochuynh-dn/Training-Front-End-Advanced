9d3d9d84747d519e1f13dfb2ec3566c4e5796301
mvn-phuochuynh-dn <phuoc.huynh@monstar-lab.com>
2021-10-01 09:39:21 +0700

    react-exercise-5

---
+++ traning-reactjs/react-exercise-5/src/App.js
@@ -0,0 +1,12 @@
+import './scss/styles.scss';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ traning-reactjs/react-exercise-5/src/components/Table/index.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+export default class Table extends Component {
+
+    renderData = () => {
+        let { dataRegister,deleteItem } = this. props;
+        return dataRegister.map( (item, index) => {
+            console.log(item);
+            return (
+                <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.birthday}</td>
+                    <td>{item.email}</td>
+                    {item.gender ==0 ? <td>Female</td> : <td>Male</td>}
+                    <td>{item.biography}</td>
+                    <td>{item.jobRole}</td>
+                    <td><button onClick={deleteItem(item.email)}>Delete</button></td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <table className="data-table">
+                <thead>
+                    <tr>
+                        <th>Index</th>
+                        <th>Name</th>
+                        <th>Birthday</th>
+                        <th>Email</th>
+                        <th>Gender</th>
+                        <th>Biography</th>
+                        <th>Job Role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderData()}
+                </tbody>
+            </table>
+        )
+    }
+}
